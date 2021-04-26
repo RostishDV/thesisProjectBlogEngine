eafaef5344c3064b586d5a3e88ce58618f9100eb
RostishDV <rostik.drizhd@gmail.com>
2021-04-26 08:18:04 +0300

    add front in vcs and controller mapping

--- src/main/java/skillbox/com/blogEngine/controllers/ApiPostController.java
+++ src/main/java/skillbox/com/blogEngine/controllers/ApiPostController.java
@@ -1,4 +1,9 @@
 package skillbox.com.blogEngine.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/post/")
 public class ApiPostController {
 }
